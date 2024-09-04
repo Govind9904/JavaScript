4e525bd88abf63175847fc6da39f126e9ad0ea12
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:50:39 +0530

    data type

---
+++ 04_datatypes-summary.js
@@ -0,0 +1,49 @@
+// Primitive data type
+// 7 categoty : string,number,boolean,null,undefined,symbol,Bug Int
+
+
+const score =100
+const scoreValue=100.13
+
+const isLoggedIn =false
+const outSideTemp =null
+
+let userEmail;
+
+
+const id =Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id === anotherId)
+console.log(id);
+console.log(anotherId);
+
+const bigNumber =26541n
+
+
+
+
+
+// Referenc /Non primitive data type
+
+// Array,objects,Function
+
+const heros=["govind","fn","voj","jfroj"];
+
+
+let myObj={
+    name :"govind",
+    age : 22
+}
+
+ const myFunction=function(){
+    console.log("hello world");
+    
+}
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// memory type
+// Stack(Primitive) , Heap(Non Primitive)
+
