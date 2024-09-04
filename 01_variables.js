514adf5374062570eac21ff697a9a09155c0b8a5
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:49:24 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId =10145;
+let accountMail="govind@gmail.com";
+var accountPassword ="125@120";
+accountity ="jaipr";
+
+// accountId =6
+// console.log(accountId);  not allowed
+
+accountMail="ogmvenn@gmial.com"
+accountPassword="1244"
+accountity="satl"
+
+console.table([accountId,accountMail,accountPassword,accountity])
