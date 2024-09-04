abe30087ef9e9fbabc1962ef587b70c4e5c6ba07
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:51:05 +0530

    string operation

---
+++ 05_strings.js
@@ -0,0 +1,20 @@
+let str = "govond"
+
+
+console.log(str.length)
+// console.log(str.substring)
+// console.log(str.toString)
+// console.log(str.charAt(5))
+// console.log(str.indexOf('v'))
+// console.log(str[0])
+
+const name = new String('govid')
+
+const  string=str.slice(-6,4)
+// console.log(string);
+
+const newName= "    JS   "
+// console.log(newName);
+console.log(newName.trim());
+//  replace pn ave 
+
