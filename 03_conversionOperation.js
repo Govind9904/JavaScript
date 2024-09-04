427aea1eb23e54f8f7dd0868fac4137c5adec42c
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:50:16 +0530

    conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score ="33"
+
+// console.log( typeof score);
+
+let valueInNmuber =Number(score);
+
+// console.log(valueInNmuber)
+
+let num =25
+let negNum=-num
+// console.log(negNum);
+
+let str1= "9"
+let str2 =3
+
+let str3= str1 + str2
+// console.log(str3);
+
+let val= "3" + 3 +3
+// console.log(val);
+
+let str = 5+ 5+"5"
+// console.log(str);
+
+let final=(3+4)*2 +5
+// console.log(final);
+
+let conv=Boolean(52)
+// console.log(conv);
+
+let num1,num2,num3
+num1=num2=num3=4
+
+console.table([num1,num2,num3])
+
