4ce3ccc2053e6f1129c601d56e83d2620ec16d56
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:49:52 +0530

    data type

---
+++ 02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+const { valueInNmuber } = require("./03_conversionOperation");
+
+ // treat all js code as a newer version
+
+// alert("Govind")  // we are using node js not browser
+
+console.log("govind");console.log(typeof valueInNmuber);
+
