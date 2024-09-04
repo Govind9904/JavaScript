b2060ef24b5bf234183b0d21c91874d8e89b1813
Govind Prajapati <168946612+Govind9904@users.noreply.github.com>
2024-09-04 10:51:34 +0530

    Add files via upload

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score =400
+
+// console.log(score);
+
+// const balance =new Number(400)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3)); // number pachi point ma value ave ex. fixed 3 lakhya che to point pachi 3 number ave 
+
+const othetNumber =23.597435
+
+// console.log(othetNumber.toPrecision(3));
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++// 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.3)); //choose a top value
+// console.log(Math.floor(4.3)); 
+// console.log(Math.floor(4.3)); 
+// console.log(Math.min(4,9,7,2,6)); 
+// console.log(Math.max(4,9,7,2,6)); 
+
+console.log(Math.random());
+console.log(Math.random()* 10 +1);
+console.log(Math.ceil(Math.random()* 10) +1);
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random () * (max - min + 1)) + min ) /// MOST IMP
